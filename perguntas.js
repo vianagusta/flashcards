1ba9d8e08afe09752b78c7528bd87a6b76f90a9c
vianagusta <vianagustavo190@gmail.com>
2024-11-13 01:03:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+/*Gustavo Viana  3ºC */
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
